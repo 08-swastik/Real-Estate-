130c16d58786d4884d841ad66d170ad4a51db071
SWASTIK <08swastik@gmail.com>
2023-05-18 00:43:45 +0530

    modified seller page

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'authentication'
 
 urlpatterns = [
-    path('seller_register',views.seller_register,name="register"),
-    path('seller_login',views.seller_login,name="login"),
+    path('seller_register/',views.seller_register,name="register"),
+    path('seller_login/',views.seller_login,name="login"),
 
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 import datetime
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm , SellerLoginForm
@@ -17,7 +18,8 @@ def seller_register(request):
             seller.save()
 
             # Perform any additional actions or redirection
-            return redirect('home')
+            messages.success(request, 'Registration successful. Please log in.')
+            return redirect('authentication:login')
     else:
         form = CustomUserCreationForm()
 
@@ -25,20 +27,48 @@ def seller_register(request):
     context = {'form': form}
     return render(request, 'authentication/register.html', context)
 
+# def seller_login(request):
+#     if request.method == 'POST':
+#         form = SellerLoginForm(data=request.POST)
+#         print(form.is_valid()) # This gives False
+#         if form.is_valid():
+#             username = form.cleaned_data.get('username')
+#             password = form.cleaned_data.get('password')
+#             user = authenticate(request, username=username, password=password)
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect('home')
+                
+#                 # Redirect to the seller's dashboard or home page
+#                 # return redirect('seller_dashboard')
+#             else:
+#                 # Invalid login credentials or the user is not a seller
+#                 messages.error(None, "Invalid login credentials")
+#     else:
+#         form = SellerLoginForm()
+
+#     context = {'form': form}
+#     return render(request, 'authentication/seller_login.html', context)
+
+from django.contrib import messages
+
 def seller_login(request):
     if request.method == 'POST':
-        form = SellerLoginForm(request.POST)
+        form = SellerLoginForm(data=request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
             user = authenticate(request, username=username, password=password)
-            if user is not None and user.is_seller:
+            if user is not None:
                 login(request, user)
-                # Redirect to the seller's dashboard or home page
-                # return redirect('seller_dashboard')
+                return redirect('home')
             else:
-                # Invalid login credentials or the user is not a seller
-                form.add_error(None, "Invalid login credentials")
+                # User is not registered
+                messages.error(request, "User is not registered. Please register as a Seller.")
+                form = SellerLoginForm()
+        else:
+            # Invalid login credentials
+            messages.error(request, "Invalid login credentials.")
     else:
         form = SellerLoginForm()
 
